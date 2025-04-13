1aa146d2c4d72dd846f2647840fa5ac9e196ef7e
Tai227 <bestcaythue007@gmail.com>
2025-04-13 21:01:24 +0700

    Delete webdatve/MovieBookingSystem directory

--- webdatve/MovieBookingSystem/cinego/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import NguoiDung, Phim, Ve, DanhGia, QuanTriVien,ChiNhanh
-
-admin.site.register(NguoiDung)
-admin.site.register(Phim)
-admin.site.register(Ve)
-admin.site.register(DanhGia)
-admin.site.register(QuanTriVien)
-admin.site.register(ChiNhanh)
--- webdatve/MovieBookingSystem/cinego/api_urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .views import phim_list
-
-urlpatterns = [
-    path('phim/', phim_list, name='api-phim'),
-]
